37ae1f219544c6469288df4340641f79aa3fa88e
Isaac Parker <isaacparker@isaacs-mbp.mynetworksettings.com>
2026-07-25 21:52:04 -0400

    haki 1.0.2

--- Formula/haki.rb
+++ Formula/haki.rb
@@ -1,26 +1,26 @@
 class Haki < Formula
   desc "Statically-typed, ARC-managed, general-purpose programming language"
   homepage "https://github.com/iceman5508/haki-lang"
-  version "1.0.1"
+  version "1.0.2"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/iceman5508/haki-lang/releases/download/v#{version}/hakic-macos-arm64.tar.gz"
-      sha256 "91e372b1514f998d17f44906b149681ac8f1d2752cc4a3680c53786c817436f8"
+      sha256 "7270bbc7f221572c18dedf3faabb01fa37e83a32e4bfa0d9f527198c943e3f17"
     else
       url "https://github.com/iceman5508/haki-lang/releases/download/v#{version}/hakic-macos-x86_64.tar.gz"
-      sha256 "b0a05a6ce804f1d85b1d49576a76b1d4968def48f8ae63de183b8be2a748fee9"
+      sha256 "7ed68b7e8af9d77ee3d94cf54d7e66d6c5598c0b72e9c66971b6d48b6bca58ed"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/iceman5508/haki-lang/releases/download/v#{version}/hakic-linux-arm64.tar.gz"
-      sha256 "31583fcf0145e5b91c743079c9e4ec28fe3a533ba44a2c39f8511fafb0daf6b0"
+      sha256 "0c478061fa12e9feff6c6c2c1a1ed6cd3d1f4e60b6943a8312c0d2a71e0dde91"
     else
       url "https://github.com/iceman5508/haki-lang/releases/download/v#{version}/hakic-linux-x86_64.tar.gz"
-      sha256 "43e0794ed604481c4284626812e88761ba51ae4ae407f9e40c5ae67919606ab4"
+      sha256 "088212a605f764a805884b4b332bc196fcb31a8af09d07d6b55fa9754623a134"
     end
   end
 
