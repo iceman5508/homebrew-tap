443cbd67dbbd7a1591e70791503f974d16e10d8e
Isaac Parker <isaacparker94@gmail.com>
2026-08-17 00:35:10 -0400

    haki 5.0.0

--- Formula/haki.rb
+++ Formula/haki.rb
@@ -1,32 +1,17 @@
 class Haki < Formula
   desc "A statically-typed, ARC-managed, general-purpose language"
   homepage "https://github.com/iceman5508/haki-lang"
-  version "2.1.0"
+  version "5.0.0"
 
   on_macos do
     on_arm do
-      url "https://github.com/iceman5508/haki-lang/releases/download/v2.1.0/hakic-macos-arm64.tar.gz"
-      sha256 "41b5d057c54477af59f0136f9f0968251d8aee75ff8b8e35385fc239c5ece488"
-    end
-    on_intel do
-      url "https://github.com/iceman5508/haki-lang/releases/download/v2.1.0/hakic-macos-x86_64.tar.gz"
-      sha256 "a9a7fef0b0b1b9f398c46201941df77e8671aecd58cf5021a253e98700b62612"
-    end
-  end
-
-  on_linux do
-    on_arm do
-      url "https://github.com/iceman5508/haki-lang/releases/download/v2.1.0/hakic-linux-arm64.tar.gz"
-      sha256 "d42280f911a1ad6ec2e16a95b8057f27778941a14e59d226669b1457b9329045"
-    end
-    on_intel do
-      url "https://github.com/iceman5508/haki-lang/releases/download/v2.1.0/hakic-linux-x86_64.tar.gz"
-      sha256 "e29982f94b6fc9d721dbb40833055b07d9422630c52eaec04fe202045317dd17"
+      url "https://github.com/iceman5508/haki-lang/releases/download/v5.0.0/hakic_v500"
+      sha256 "78c2e34c5b61669674c6559b1e09218098f853ae658adda51f3c4805f9ad5bfb"
     end
   end
 
   def install
-    bin.install "hakic"
+    bin.install "hakic_v500" => "hakic"
     bin.install_symlink "hakic" => "haki"
     bin.install_symlink "hakic" => "haki-gtk"
     bin.install_symlink "hakic" => "haki-dom"
